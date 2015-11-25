d66d06dbba0fdec369cfb70d834407d223309708
Eugene ONeill <oneill.eugene@gmail.com>
2015-11-25 08:40:40 -0800

    fix: test should wait until finished

--- tasks/test.js
+++ tasks/test.js
@@ -5,19 +5,22 @@ var nodetree = require("nodetree");
 var path = require("path");
 
 module.exports = function(gulp, depends) {
-  var rootDir = "test";
+  var rootDir = path.resolve("./test");
   gulp.task("test", depends, function() {
-    var site = new SiteBuilder({
-      root: rootDir,
-      source: "src"
-    });
-    var metalsmith = site.create();
+    return new Promise(function(reject, resolve) {
+      var site = new SiteBuilder({
+        root: rootDir,
+        source: "src"
+      });
+      var metalsmith = site.create();
 
-    metalsmith.build(function(err) {
-      if (err) {
-        throw err;
-      }
-      nodetree(path.join(rootDir, site.config.dest));
+      metalsmith.build(function(err) {
+        if (err) {
+          reject(err);
+        }
+        nodetree(path.join(rootDir, site.config.dest));
+        resolve();
+      });
     });
   });
 };
